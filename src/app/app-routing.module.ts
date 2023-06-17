cf42fa4157d83fd5f6ffa8f722e420d8173038df
Yaroslav Vasilyev <yaroslavvas49@gmail.com>
2023-06-17 17:16:45 +0300

    delete route
    There's no point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CatSearchComponent } from './components/cat-search/cat-search.component';
 
 const routes: Routes = [
-  { path: 'cat-search', component: CatSearchComponent },
-  { path: '', redirectTo: '/cat-search', pathMatch: 'full' }
 ];
 
 @NgModule({
